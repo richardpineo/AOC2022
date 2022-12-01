405721245cdf5fb89c1376409ac2ef464d84abc9
Richard Pineo <Richard.E.Pineo@ge.com>
2022-11-30 19:39:01 -0800

    Initial Commit

---
+++ AOC2022/AOC2022App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2022App.swift
+//  AOC2022
+//
+//  Created by Richard Pineo on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct AOC2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
