5db9b67e44b94670d13335b2f353fadde0184629
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-02 21:46:23 -0800

    3b

--- AOC2022/Puzzles/3/Solve3.swift
+++ AOC2022/Puzzles/3/Solve3.swift
@@ -8,11 +8,11 @@ class Solve3: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example3") == 0
+		solveB("Example3") == 70
 	}
 
-	var answerA = ""
-	var answerB = ""
+	var answerA = "8176"
+	var answerB = "2689"
 
 	func solveA() -> String {
 		solveA("Input3").description
@@ -21,11 +21,11 @@ class Solve3: PuzzleSolver {
 	func solveB() -> String {
 		solveB("Input3").description
 	}
-	
+
 	struct Rucksack {
 		var pocket1: String
 		var pocket2: String
-		
+
 		var commonItem: Character {
 			let found = pocket1.first {
 				pocket2.contains($0)
@@ -33,7 +33,7 @@ class Solve3: PuzzleSolver {
 			return found!
 		}
 	}
-	
+
 	func score(of item: Character) -> Int {
 		let itemVal = item.asciiValue!
 		let aVal = Character("a").asciiValue!
@@ -45,16 +45,34 @@ class Solve3: PuzzleSolver {
 
 	func solveA(_ fileName: String) -> Int {
 		let rucksacks: [Rucksack] = FileHelper.load(fileName)!.filter { !$0.isEmpty }.map {
-			return .init(
-				pocket1: $0.subString(start: 0, count: $0.count/2),
-				pocket2: $0.subString(start: $0.count/2, count: $0.count/2))
+			.init(
+				pocket1: $0.subString(start: 0, count: $0.count / 2),
+				pocket2: $0.subString(start: $0.count / 2, count: $0.count / 2)
+			)
 		}
 		return rucksacks.reduce(0) {
 			$0 + score(of: $1.commonItem)
 		}
 	}
-	
+
+	struct ElfGroup {
+		var members: [String]
+		var commonItem: Character {
+			members[0].filter {
+				members[1].contains($0) && members[2].contains($0)
+			}.first!
+		}
+	}
+
 	func solveB(_ fileName: String) -> Int {
-		0
+		let rawElves = FileHelper.load(fileName)!.filter { !$0.isEmpty }
+		var elfGroups: [ElfGroup] = []
+		for groupStart in stride(from: 0, to: rawElves.count, by: 3) {
+			elfGroups.append(.init(members: Array(rawElves[groupStart ... (groupStart + 2)])))
+		}
+
+		return elfGroups.reduce(0) {
+			$0 + score(of: $1.commonItem)
+		}
 	}
 }
