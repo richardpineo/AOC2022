fde1cbdab2fbe3139581cc9c32b2c52be674427a
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-02 21:50:36 -0800

    3done

--- AOC2022/Puzzles/3/Solve3.swift
+++ AOC2022/Puzzles/3/Solve3.swift
@@ -22,18 +22,6 @@ class Solve3: PuzzleSolver {
 		solveB("Input3").description
 	}
 
-	struct Rucksack {
-		var pocket1: String
-		var pocket2: String
-
-		var commonItem: Character {
-			let found = pocket1.first {
-				pocket2.contains($0)
-			}
-			return found!
-		}
-	}
-
 	func score(of item: Character) -> Int {
 		let itemVal = item.asciiValue!
 		let aVal = Character("a").asciiValue!
@@ -43,6 +31,15 @@ class Solve3: PuzzleSolver {
 		return Int(itemVal - startCharacter + 1)
 	}
 
+	struct Rucksack {
+		var pocket1: String
+		var pocket2: String
+
+		var commonItem: Character {
+			pocket1.first { pocket2.contains($0) }!
+		}
+	}
+
 	func solveA(_ fileName: String) -> Int {
 		let rucksacks: [Rucksack] = FileHelper.load(fileName)!.filter { !$0.isEmpty }.map {
 			.init(
@@ -50,29 +47,21 @@ class Solve3: PuzzleSolver {
 				pocket2: $0.subString(start: $0.count / 2, count: $0.count / 2)
 			)
 		}
-		return rucksacks.reduce(0) {
-			$0 + score(of: $1.commonItem)
-		}
+		return rucksacks.reduce(0) { $0 + score(of: $1.commonItem) }
 	}
 
 	struct ElfGroup {
 		var members: [String]
 		var commonItem: Character {
-			members[0].filter {
-				members[1].contains($0) && members[2].contains($0)
-			}.first!
+			members[0].first { members[1].contains($0) && members[2].contains($0) }!
 		}
 	}
 
 	func solveB(_ fileName: String) -> Int {
 		let rawElves = FileHelper.load(fileName)!.filter { !$0.isEmpty }
-		var elfGroups: [ElfGroup] = []
-		for groupStart in stride(from: 0, to: rawElves.count, by: 3) {
-			elfGroups.append(.init(members: Array(rawElves[groupStart ... (groupStart + 2)])))
+		let elfGroups: [ElfGroup] = stride(from: 0, to: rawElves.count, by: 3).map {
+			.init(members: Array(rawElves[$0 ... ($0 + 2)]))
 		}
-
-		return elfGroups.reduce(0) {
-			$0 + score(of: $1.commonItem)
-		}
+		return elfGroups.reduce(0) { $0 + score(of: $1.commonItem) }
 	}
 }
