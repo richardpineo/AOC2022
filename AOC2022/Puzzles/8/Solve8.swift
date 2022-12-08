5cf940e768924101461dc317607bfe6d8ac2b2a4
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-07 23:47:26 -0800

    8a

--- AOC2022/Puzzles/8/Solve8.swift
+++ AOC2022/Puzzles/8/Solve8.swift
@@ -4,11 +4,11 @@ import Foundation
 
 class Solve8: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		solveA("Example7") == 21
+		solveA("Example8") == 21
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example7") == 0
+		solveB("Example8") == 0
 	}
 
 	var answerA = ""
@@ -21,10 +21,28 @@ class Solve8: PuzzleSolver {
 	func solveB() -> String {
 		solveB("Input8").description
 	}
-
+	
+	func isVisibleFrom(_ pos: Position2D, _ move: Position2D, _ grid: Grid2D) -> Bool {
+		let val = grid.value(pos)
+		var nextPos = pos.offset(move)
+		while(grid.valid(nextPos)) {
+			if grid.value(nextPos) >= val {
+				return false
+			}
+			nextPos = nextPos.offset(move)
+		}
+		return true
+	}
+	
+	func isVisible(_ pos: Position2D, _ grid: Grid2D) -> Bool {
+		let offsets: [Position2D] = [.init(1, 0), .init(-1, 0), .init(0,1), .init(0,-1)]
+		let found = offsets.first { isVisibleFrom(pos, $0, grid ) }
+		return found != nil
+	}
+	
 	func solveA(_ fileName: String) -> Int {
-		let lines = FileHelper.load(fileName)!.filter { $0.isEmpty }
-		return lines.count
+		let grid = Grid2D(fileName: fileName)
+		return grid.allPositions.filter { isVisible($0, grid) }.count
 	}
 
 	func solveB(_ fileName: String) -> Int {
