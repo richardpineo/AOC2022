149e8f6c9af7f9a2970391a786f75bff839fc13d
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-10 13:12:51 -0800

    10 load

--- AOC2022/Puzzles/10/Solve10.swift
+++ AOC2022/Puzzles/10/Solve10.swift
@@ -21,9 +21,25 @@ class Solve10: PuzzleSolver {
 	func solveB() -> String {
 		solveB("Input10").description
 	}
+	
+	enum Command {
+		case noop
+		case addx(Int)
+	}
+	
+	func load(_ fileName: String) -> [Command] {
+		FileHelper.loadAndTokenize(fileName).compactMap {
+			switch $0[0] {
+			case "noop": return .noop
+			case "addx": return .addx(Int($0[1])!)
+			default: return nil
+			}
+		}
+	}
 
 	func solveA(_ fileName: String) -> Int {
-		0
+		let commands = load(fileName)
+		return commands.count
 	}
 
 	func solveB(_ fileName: String) -> Int {
