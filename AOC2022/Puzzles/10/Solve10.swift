8dbe5327ffbb580b0bfa67cf9d75226556b72ed1
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-10 13:48:20 -0800

    10a

--- AOC2022/Puzzles/10/Solve10.swift
+++ AOC2022/Puzzles/10/Solve10.swift
@@ -4,6 +4,7 @@ import Foundation
 
 class Solve10: PuzzleSolver {
 	func solveAExamples() -> Bool {
+		execute(commands: [.noop, .addx(3), .addx(-5)]).x(at: 6) == -1 &&
 		solveA("Example10") == 13140
 	}
 
@@ -11,7 +12,7 @@ class Solve10: PuzzleSolver {
 		solveB("Example10") == 1
 	}
 
-	var answerA = ""
+	var answerA = "11720"
 	var answerB = ""
 
 	func solveA() -> String {
@@ -27,6 +28,34 @@ class Solve10: PuzzleSolver {
 		case addx(Int)
 	}
 	
+	class CPU {
+		private var xAtCycle: [Int: Int] = [0: 1]
+		private var cycle: Int = 0
+		private var x: Int = 1
+		
+		func x(at cycle: Int) -> Int {
+			var xVal = 0
+			for index in 0..<cycle {
+				if let val = xAtCycle[index] {
+					xVal = val
+				}
+			}
+			return xVal
+		}
+		
+		func execute( _ command: Command) {
+			cycle += 1
+			switch command {
+			case .noop:
+				break
+			case let .addx(xVal):
+				cycle += 1
+				x += xVal
+				xAtCycle[cycle] = x
+			}
+		}
+	}
+	
 	func load(_ fileName: String) -> [Command] {
 		FileHelper.loadAndTokenize(fileName).compactMap {
 			switch $0[0] {
@@ -36,10 +65,23 @@ class Solve10: PuzzleSolver {
 			}
 		}
 	}
-
+	
+	func execute(commands: [Command]) -> CPU {
+		let cpu = CPU()
+		commands.forEach {
+			cpu.execute($0)
+		}
+		return cpu
+	}
+	
 	func solveA(_ fileName: String) -> Int {
-		let commands = load(fileName)
-		return commands.count
+		let cpu = execute(commands: load(fileName))
+		
+		let keyStrengths = stride(from: 20, to: 221, by: 40).map {
+			($0, cpu.x(at: $0), $0 * cpu.x(at: $0))
+		}
+
+		return keyStrengths.map{ $0.2 }.reduce(0, +)
 	}
 
 	func solveB(_ fileName: String) -> Int {
