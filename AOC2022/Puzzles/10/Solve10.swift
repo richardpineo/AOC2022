0ac7f3c94c51e38ca73c8871fe724dec225d634c
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-10 13:09:00 -0800

    10 skele

---
+++ AOC2022/Puzzles/10/Solve10.swift
@@ -0,0 +1,32 @@
+
+import AOCLib
+import Foundation
+
+class Solve10: PuzzleSolver {
+	func solveAExamples() -> Bool {
+		solveA("Example10") == 13140
+	}
+
+	func solveBExamples() -> Bool {
+		solveB("Example10") == 1
+	}
+
+	var answerA = ""
+	var answerB = ""
+
+	func solveA() -> String {
+		solveA("Input10").description
+	}
+
+	func solveB() -> String {
+		solveB("Input10").description
+	}
+
+	func solveA(_ fileName: String) -> Int {
+		0
+	}
+
+	func solveB(_ fileName: String) -> Int {
+		0
+	}
+}
