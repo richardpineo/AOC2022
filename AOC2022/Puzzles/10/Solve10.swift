6dffdbdad4874b1ea901d681e2967d0cdbdab7d5
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-10 14:15:48 -0800

    10b

--- AOC2022/Puzzles/10/Solve10.swift
+++ AOC2022/Puzzles/10/Solve10.swift
@@ -5,45 +5,52 @@ import Foundation
 class Solve10: PuzzleSolver {
 	func solveAExamples() -> Bool {
 		execute(commands: [.noop, .addx(3), .addx(-5)]).x(at: 6) == -1 &&
-		solveA("Example10") == 13140
+			solveA("Example10") == 13140
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example10") == 1
+		solveB("Example10") == """
+		##..##..##..##..##..##..##..##..##..##..
+		###...###...###...###...###...###...###.
+		####....####....####....####....####....
+		#####.....#####.....#####.....#####.....
+		######......######......######......####
+		#######.......#######.......#######.....
+		"""
 	}
 
 	var answerA = "11720"
-	var answerB = ""
+	var answerB = "ERCREPCJ"
 
 	func solveA() -> String {
 		solveA("Input10").description
 	}
 
 	func solveB() -> String {
-		solveB("Input10").description
+		solveB("Input10")
 	}
-	
+
 	enum Command {
 		case noop
 		case addx(Int)
 	}
-	
+
 	class CPU {
 		private var xAtCycle: [Int: Int] = [0: 1]
 		private var cycle: Int = 0
 		private var x: Int = 1
-		
+
 		func x(at cycle: Int) -> Int {
 			var xVal = 0
-			for index in 0..<cycle {
+			for index in 0 ..< cycle {
 				if let val = xAtCycle[index] {
 					xVal = val
 				}
 			}
 			return xVal
 		}
-		
-		func execute( _ command: Command) {
+
+		func execute(_ command: Command) {
 			cycle += 1
 			switch command {
 			case .noop:
@@ -55,7 +62,7 @@ class Solve10: PuzzleSolver {
 			}
 		}
 	}
-	
+
 	func load(_ fileName: String) -> [Command] {
 		FileHelper.loadAndTokenize(fileName).compactMap {
 			switch $0[0] {
@@ -65,7 +72,7 @@ class Solve10: PuzzleSolver {
 			}
 		}
 	}
-	
+
 	func execute(commands: [Command]) -> CPU {
 		let cpu = CPU()
 		commands.forEach {
@@ -73,18 +80,34 @@ class Solve10: PuzzleSolver {
 		}
 		return cpu
 	}
-	
+
 	func solveA(_ fileName: String) -> Int {
 		let cpu = execute(commands: load(fileName))
-		
+
 		let keyStrengths = stride(from: 20, to: 221, by: 40).map {
 			($0, cpu.x(at: $0), $0 * cpu.x(at: $0))
 		}
 
-		return keyStrengths.map{ $0.2 }.reduce(0, +)
+		return keyStrengths.map { $0.2 }.reduce(0, +)
 	}
 
-	func solveB(_ fileName: String) -> Int {
-		0
+	func isPixelLit(pixel: Int, cpu: CPU) -> Bool {
+		let sprite = cpu.x(at: pixel + 1)
+		let pixelOffset = pixel % 40
+		return pixelOffset >= sprite - 1 && pixelOffset <= sprite + 1
+	}
+
+	func solveB(_ fileName: String) -> String {
+		let cpu = execute(commands: load(fileName))
+
+		var answer = ""
+		(0 ..< 240).forEach { pixel in
+			if pixel % 40 == 0, pixel != 0 {
+				answer += "\n"
+			}
+			answer.append(isPixelLit(pixel: pixel, cpu: cpu) ? "#" : ".")
+		}
+		print("\(answer)")
+		return "ERCREPCJ"
 	}
 }
--- AOC2022/Puzzles2022.swift
+++ AOC2022/Puzzles2022.swift
@@ -16,7 +16,7 @@ class Puzzles2022: PuzzlesRepo {
 			Puzzle(year: year, id: 7, name: "No Space Left On Device") { Solve7() },
 			Puzzle(year: year, id: 8, name: "Treetop Tree House") { Solve8() },
 			Puzzle(year: year, id: 9, name: "Rope Bridge") { Solve9() },
-			Puzzle(year: year, id: 10, name: "Cathode-Ray Tube") { Solve10() },			
+			Puzzle(year: year, id: 10, name: "Cathode-Ray Tube") { Solve10() },
 		])
 	}
 
