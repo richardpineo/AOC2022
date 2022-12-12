f39f77868da9694e8ca0c7b7b991f2af95311298
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-11 22:15:50 -0800

    can load

--- AOC2022/Puzzles/12/Solve12.swift
+++ AOC2022/Puzzles/12/Solve12.swift
@@ -22,12 +22,39 @@ class Solve12: PuzzleSolver {
 		solveB("Input12").description
 	}
 	
+	class Hill {
+		init(fileName: String) {
+			let lines = FileHelper.load(fileName)!.filter{ !$0.isEmpty }
+			heights = .init(maxPos: .init( lines[0].count, lines.count), initialValue: 0)
+			start = .origin
+			end = .origin
+
+			for y in 0 ..< lines.count {
+				for x in 0 ..< lines[y].count {
+					let value = lines[y].character(at: x)
+					if value == "S" {
+						start = .init(x, y)
+					} else if value == "E" {
+						end = .init(x, y)
+					} else {
+						heights.setValue(.init(x, y), Int(value.asciiValue! - Character("a").asciiValue!))
+					}
+				}
+			}
+		}
+		
+		var heights: Grid2D
+		var start: Position2D
+		var end: Position2D
+	}
+	
 	func solveA(_ fileName: String) -> Int {
-		let lines = FileHelper.load(fileName)!.filter { $0.isEmpty }
-		return lines.count
+		let hill = Hill(fileName: fileName)
+		
+		return 0
 	}
 	
 	func solveB(_ fileName: String) -> Int {
 		0
-	}	
+	}
 }
