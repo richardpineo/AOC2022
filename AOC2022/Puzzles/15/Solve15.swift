dadbae9cfb2894912556ddb29a0ac5c8d112ea08
Richard Pineo <Richard.E.Pineo@ge.com>
2022-12-20 16:58:11 -0800

    15a

--- AOC2022/Puzzles/15/Solve15.swift
+++ AOC2022/Puzzles/15/Solve15.swift
@@ -4,7 +4,7 @@ import Foundation
 
 class Solve15: PuzzleSolver {
 	func solveAExamples() -> Bool {
-		solveA("Example15") == 26
+		solveA("Example15", row: 10) == 26
 	}
 
 	func solveBExamples() -> Bool {
@@ -15,20 +15,34 @@ class Solve15: PuzzleSolver {
 	var answerB = ""
 
 	func solveA() -> String {
-		solveA("Input15").description
+		solveA("Input15", row: 2_000_000).description
 	}
 
 	func solveB() -> String {
 		solveB("Input15").description
 	}
-	
+
 	struct Sensor {
 		var position: Position2D
 		var beacon: Position2D
+
+		func covered(_ other: Position2D) -> Bool {
+//			if other == position || other == beacon {
+//				return false
+			//		}
+			let maxDistance = position.cityDistance(beacon)
+			return abs(other.x - position.x) + abs(other.y - position.y) <= maxDistance
+		}
+
+		var boundingBox: (Position2D, Position2D) {
+			let maxDistance = position.cityDistance(beacon)
+			return (.init(position.x - maxDistance, position.y - maxDistance),
+			        .init(position.x + maxDistance, position.y + maxDistance))
+		}
 	}
-	
+
 	func load(_ fileName: String) -> [Sensor] {
-		let lines = FileHelper.loadAndTokenize (fileName)
+		let lines = FileHelper.loadAndTokenize(fileName)
 		return lines.map { tokens in
 			let posX = tokens[2].subString(start: 2, count: tokens[2].count - 3)
 			let posY = tokens[3].subString(start: 2, count: tokens[3].count - 3)
@@ -36,17 +50,39 @@ class Solve15: PuzzleSolver {
 			let beaconY = tokens[9].subString(start: 2, count: tokens[9].count - 2)
 			return .init(
 				position: .init(Int(posX)!, Int(posY)!),
-				beacon: .init(Int(beaconX)!, Int(beaconY)!))
+				beacon: .init(Int(beaconX)!, Int(beaconY)!)
+			)
 		}
 	}
 
-	func solveA(_ fileName: String) -> Int {
+	func solveA(_ fileName: String, row: Int) -> Int {
 		let sensors = load(fileName)
-		
-		return sensors.count
+
+		var minX = Int.max
+		var maxX = Int.min
+		sensors.forEach {
+			let bBox = $0.boundingBox
+			minX = min(minX, bBox.0.x)
+			maxX = max(maxX, bBox.1.x)
+		}
+		var count = 0
+		for x in minX ... maxX {
+			let pos: Position2D = .init(x, row)
+			let notBeacon = sensors.allSatisfy {
+				$0.beacon != pos
+			}
+			if notBeacon {
+				let covered = !sensors.allSatisfy {
+					!$0.covered(pos) && $0.beacon != pos
+				}
+				count += covered ? 1 : 0
+			}
+		}
+
+		return count
 	}
 
-	func solveB(_ fileName: String) -> Int {
+	func solveB(_: String) -> Int {
 		0
 	}
 }
